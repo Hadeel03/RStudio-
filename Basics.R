904def3d4cfdf99bb8de4b76484a5552f54dc473
Ghadeer Al Jufout <ghadeer1112@yahoo.com>
2024-10-01 20:29:06 -0700

    Create Basics.R
    This script provides a quick overview of R syntax, demonstrating object assignment, vector creation, and basic functions.

---
+++ Basics.R
@@ -0,0 +1,12 @@
+print("hello woRld") 
+print(4)
+my_apples <- 4 # Object assignment operator... shortcut on Mac (Option + -)
+my_apples - 1 # R language is case sensitive
+n_apples <- c(7, 10, 3)
+n_apples <- c(7, my_apples, 3)
+names <- c("Menglin", "Gloria", "Robert") # Whatever in quotes is not defined in R
+names
+data.frame(person = names, apple_count = n_apples)
+friends <- data.frame(person = names, apple_count = n_apples)
+
+# if you need help use ? followed by function or object name
